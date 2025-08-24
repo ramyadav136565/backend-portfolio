9a842ad29991dfd1d8cde19979d51d11fa800a0c
Rameshwar Yadav <ramy@Rameshwars-MacBook-Air-2.local>
2025-08-24 21:25:11 +0530

    chat_bot_api

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 import tempfile
+from openai import OpenAI
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
@@ -27,3 +30,31 @@ def generate_pdf(data: dict):
         f.write(text)
 
     return FileResponse(pdf_path, filename="output.pdf", media_type="application/pdf")
+
+
+
+
+
+# Request schema
+class MessageRequest(BaseModel):
+    message: str
+
+# OpenAI-compatible client
+client = OpenAI(
+    api_key="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",
+    base_url="https://openrouter.ai/api/v1"
+)
+
+@app.post("/chat_bot_api")
+async def chat_bot_api(req: MessageRequest):
+    prompt = [
+        {"role": "system", "content": "You are a helpful assistant.answer the question concisely."},
+        {"role": "user", "content": req.message}
+    ]
+
+    response = client.chat.completions.create(
+        model="google/gemma-3-12b-it:free",
+        messages=prompt
+    )
+
+    return {"response": response.choices[0].message["content"].strip()}
